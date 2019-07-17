e41c0d8c4fa5ab9f98a8eb10f0517d6df1dd61ae
Danilo GingaOne <danilo.paixao@gingaone.com>
2019-07-17 17:51:11 -0300

    initial commit

---
+++ src/main/java/br/com/danilopaixao/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package br.com.danilopaixao.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//mvn spring-boot:run
+//http://localhost:8761/
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
